8d7c95fafc7fd017839679286b2d6ce3e461c89a
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-11-15 11:28:22 -0300

    feat: add image

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Form } from './components/Form/Form';
 import { Output } from './components/Output/Output';
+import Truck from './images/Caminhao.png';
 import './styles/app.scss';
 
 function App() {
     return (
         <div className="app-container">
-            <div className="container">
+            <div className="wrapper">
                 <header className="title-container">
                     <h1>Busca Cep</h1>
                     <p>Encontre endereços em segundos!</p>
                 </header>
                 <Form />
                 <Output />
+                <img src={Truck} alt="caminhão" />
             </div>
         </div>
     );
