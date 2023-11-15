e20d6aa69032f0affb95cf7a782a5d7a6f3f1a85
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-11-15 15:32:05 -0300

    feat: create http request to viacep

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { Form } from './components/Form/Form';
 import { Output } from './components/Output/Output';
 import Truck from './images/Caminhao.png';
+
+import { LocationType } from './types/locationTypes';
 import './styles/app.scss';
 
 function App() {
+    const [location, setLocation] = useState<LocationType>();
+
+    console.log(location);
+
     return (
         <div className="app-container">
             <div className="wrapper">
@@ -11,7 +18,7 @@ function App() {
                     <h1>Busca Cep</h1>
                     <p>Encontre endereços em segundos!</p>
                 </header>
-                <Form />
+                <Form setLocation={setLocation} />
                 <Output />
                 <img src={Truck} alt="caminhão" />
             </div>
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,40 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
 import { MagnifyingGlass } from '@phosphor-icons/react';
+import { LocationType } from '../../types/locationTypes';
 import './_Form.scss';
+interface FormProps {
+    setLocation: (data: LocationType) => void;
+}
 
-export const Form = () => {
+export const Form = ({ setLocation }: FormProps) => {
+    const [cep, setCep] = useState('');
+    const [error, setError] = useState(false);
+
+    const handleChangeCep = (e: ChangeEvent<HTMLInputElement>) => {
+        setCep(e.target.value);
+    };
+
+    const handleCepRequest = async (e: FormEvent) => {
+        e.preventDefault();
+
+        if (cep === '') {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+        const response = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+        setLocation(data);
+    };
+
     return (
-        <form>
-            <input type="number" placeholder="insira um cep" />
-            <span>
+        <form onSubmit={handleCepRequest}>
+            <input onChange={handleChangeCep} value={cep} type="text" placeholder="insira um cep" />
+            <button type="submit">
                 <MagnifyingGlass size={30} />
-            </span>
+            </button>
+            <span className="error">{error && 'O campo deve ser preenchido'}</span>
         </form>
     );
 };
